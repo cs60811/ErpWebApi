bcb34f010b429b6e95c2d0bc522dc3b77fa27d57
Chang Kang Hsieh <cs60811@gmail.com>
2026-01-03 23:58:51 +0800

    Configure base path for GitHub Pages and prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ErpWebApi/',
   plugins: [react()],
   server: {
     allowedHosts: ["gutturalized-explanatory-jeff.ngrok-free.dev"],
